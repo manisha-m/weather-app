cc7f48699d5dd29f1b7b30784c60278b5f8e3b62
Manisha <manisha.malla@gmail.com>
2016-06-21 18:50:37 +0530

    Ready the app for Heroku deployment

---
+++ deploy.js
@@ -0,0 +1,17 @@
+var isProduction = true;
+
+// Since postinstall will also run when you run npm install
+// locally we make sure it only runs in production
+if (isProduction) {
+
+  // We basically just create a child process that will run
+  // the production bundle command
+  var child_process = require('child_process');
+  child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+}
